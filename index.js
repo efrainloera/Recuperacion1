8dd782a6ec0bfc49dae66d94b754376f1646a592
efrainloera <efrainloerub578@gmail.com>
2022-06-08 17:00:23 -0500

    Add new filter method exercise

---
+++ index.js
@@ -0,0 +1,7 @@
+//Map es una función que te permite transformar los 
+//elementos de una lista y que devuelve una nueva lista con los elementos transformados.
+
+
+const ejemploUno = listaOriginal.map(function callback(elementoActual, index) {
+    return /* Elemento devuelto de nuevaLista */;
+  });
